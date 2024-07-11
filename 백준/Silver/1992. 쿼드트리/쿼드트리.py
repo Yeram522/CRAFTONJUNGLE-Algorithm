b005b1e4907d2942bc3a2698043a9ae4bff20df2
Nongdam <63442636+Yeram522@users.noreply.github.com>
2024-07-11 20:32:43 +0900

    [Silver I] Title: 쿼드트리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -1,30 +1,35 @@
-import sys
 #input
-N = int(sys.stdin.readline())# 영상 크기
+N = int(input()) # 영상 크기
 
-W= [sys.stdin.readline() for _ in range(N)]# 정리 해두기 N행 배열 입력 받는 법
+W= [list(map(int, input().strip())) for _ in range(N)]# 정리 해두기 N행 배열 입력 받는 법
 
 result = list()   #문자열 리스트이다.
 
 
 def quad_tree(x,y, div): # x,y 좌표, 나누는 분기
-    color = int(W[x][y])
+    color = W[x][y]
+    trigger = False
     
     for i in range(x,x + div):
         for j in range(y,y + div):
-            if color != int(W[i][j]):
-                result.append('(')
-                quad_tree(x,y, div//2)
-                quad_tree(x,y+div//2, div//2)
-                quad_tree(x+div//2,y, div//2)
-                quad_tree(x+div//2,y+div//2, div//2)
-                result.append(')')
-                return
-            
+            if color != W[i][j]:
+                trigger = True
+                
+                break
+           
+    if trigger:
+        print('(', end='')
+        quad_tree(x,y, div//2)
+        quad_tree(x,y+div//2, div//2)
+        quad_tree(x+div//2,y, div//2)
+        quad_tree(x+div//2,y+div//2, div//2)
+        print(')', end='')
+        return
+        
     if color == 1:
-        result.append('1')
+        print(1, end='')
     else:
-        result.append('0')
+        print(0, end='')
     return 
                 
     
