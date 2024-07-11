f504aee74c445728f07a117705d738afff5efc61
Nongdam <63442636+Yeram522@users.noreply.github.com>
2024-07-11 11:00:38 +0900

    [Silver I] Title: 쿼드트리, Time: 128 ms, Memory: 112576 KB -BaekjoonHub

---
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -0,0 +1,36 @@
+
+#input
+N = int(input()) # 영상 크기
+
+W= [list(map(int, input().strip())) for _ in range(N)] # 정리 해두기 N행 배열 입력 받는 법
+
+result = list()   #문자열 리스트이다.
+
+
+def quad_tree(x,y, div): # x,y 좌표, 나누는 분기
+    color = W[x][y]
+    
+    for i in range(x,x + div):
+        for j in range(y,y + div):
+            if color != W[i][j]:
+                result.append('(')
+                quad_tree(x,y, div//2)
+                quad_tree(x,y+div//2, div//2)
+                quad_tree(x+div//2,y, div//2)
+                quad_tree(x+div//2,y+div//2, div//2)
+                result.append(')')
+                return
+            
+    if color == 1:
+        result.append('1')
+    else:
+        result.append('0')
+    return 
+                
+    
+                       
+quad_tree(0,0,N)
+
+
+for r in result:
+    print(r, end='')
