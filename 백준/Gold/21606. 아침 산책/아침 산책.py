5a0931a1496c568ac8cc277a6b37414a4ae93188
Nongdam <63442636+Yeram522@users.noreply.github.com>
2024-07-15 14:29:21 +0900

    [Gold III] Title: 아침 산책, Time: 760 ms, Memory: 158384 KB, Score: 108 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,5 +1,4 @@
 import sys
-from collections import defaultdict
 input=sys.stdin.readline # input함수 바꾸기
 
 V = int(input())
@@ -16,32 +15,62 @@ for i in range(V-1):
     Tree[u].append(v)
     Tree[v].append(u)
  
-def DFS(start):
-    count = 0
-    stack = [start] # 스택을 이용한 DFS
+# def DFS(start):
+#     count = 0
+#     stack = [start] # 스택을 이용한 DFS
 
-    while stack:
-        node = stack.pop() # 해당 지점과 이어진 노드들 리스트
-        for next in Tree[node]:
-            if place[next-1] == "0": # 실외일 경우
-                stack.append(next)
-            else: # 실내일 경우에는 해당 루트의 산책을 종료한다.
-                #같은게 나올수있나?
-                if(next == start): continue
-                count+=1
-                #print(start, next)
+#     while stack:
+#         node = stack.pop() # 해당 지점과 이어진 노드들 리스트
+#         for next in Tree[node]:
+#             if place[next-1] == "0": # 실외일 경우
+#                 stack.append(next)
+#             else: # 실내일 경우에는 해당 루트의 산책을 종료한다.
+#                 #같은게 나올수있나?
+#                 if(next == start): continue
+#                 count+=1
+#                 print(start, next)
                 
     
-    return count
+#     return count
 
+result = 0
+visited = set()
+
+def DFS(start,visited):
+    global result
+    
+    for next in Tree[start]:
+        if place[next-1] == "0": # 실외라면
+            if next not in visited:
+                visited.add(next)
+                DFS(next,visited)
+                visited.remove(next)
+        else:
+            
+            # 실내이기때문에
+            if next not in visited:
+                #print(visited)
+                result += 1
+            
+    return
+            
 
 # 시작점과 끝점을 정해야한다. start와 end가 정해지면 경로는 유일.
 # for문으로 시작점을 먼저 정해서 시작하면 중복이 일어나지 않음!
-result = 0
+
 for vertex in Tree.keys():# 실내인 경우에만 start지점에 포함한다.
     if place[vertex-1] == "1":
-        result += DFS(vertex)
+        visited = set()
+        visited.add(vertex)
+        DFS(vertex,visited)
         
         
 print(result)
         
+    
+# subtask
+# 1 O
+# 2 시간초과
+# 3 메모리초과 
+# 4 시간초과  N<= 1000
+# 5 메모리초과
