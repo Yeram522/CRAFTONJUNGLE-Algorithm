1828311dbc3c0f39a555f0a8e00f2c67bbc204e8
Nongdam <63442636+Yeram522@users.noreply.github.com>
2024-12-05 21:44:17 +0900

    [Silver III] Title: 바이러스, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,41 +1,33 @@
 import sys
-import collections
-input=sys.stdin.readline # input함수 바꾸기
+input = sys.stdin.readline
 
-N = int(input()) # 컴퓨터 수
+N = int(input()) # 컴퓨터의 수
 M = int(input()) # 컴퓨터 쌍의 수
 
-Graph = {i: [] for i in range(1, N+1)}
+Graph = [ list() for _ in range(N+1) ]  # 인접 리스트
 
-for j in range(M):
-    u, v = map(int, input().split())
+for _ in range(M):
+    a,b = map(int, input().split())
     
-    Graph[u].append(v)
-    Graph[v].append(u)
+    Graph[a].append(b)
+    Graph[b].append(a)
     
-#1번 컴터씨가 웜바이러스에 걸려부림 ㅉㅉ
+# DFS
+visited = [0 for _ in range(N+1)]
 
-def BFS(node):
-    global visited
-    q = collections.deque([node])
-    ret = 0
+queue = list()
+queue.append(1) # 처음 시작은 1번 컴퓨터
+visited[1] = 1 # 1번 방문처리
+count = 0 # 1번 컴퓨터는 제외한 카운트
+while(len(queue) != 0):
+    idx = queue.pop(0)
     
-    while q:
-        vertex = q.popleft()
+    for com in Graph[idx]:
+        if(visited[com] == 0):
+            visited[com] = 1
+            count += 1 # 감염 카운트 up
+            queue.append(com)
         
-        for next in Graph[vertex]:
-            if next not in visited:
-                visited.add(next)
-                ret += 1
-                q.append(next)
-                
-                
-    return ret
-            
-            
-    
-
-
-visited = set([1])
-
-print(BFS(1))
+        # 방문한적 있다면 방문하지 않는다.
+        
+print(count)
